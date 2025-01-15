ab7565e9ddae4b7e9b56871f005037a16a11ac72
Varun Pandey <varunp5324@gmail.com>
2025-01-16 03:23:28 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import './App.css'
-import Layout from "./pages/layout.jsx";
+import Layout from "./pages/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import Whychoose from "./pages/Whychoose.jsx";
 import BandA from "./pages/BandA,jsx";
