a7bdb0c0f87a882be1eeb6edc96977dbdbeba805
Varun Pandey <varunp5324@gmail.com>
2025-01-18 02:11:49 +0530

    first commit

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -19,7 +19,13 @@ const Layout = () => {
                 {(!ShowNav)
                     ? <>
                         <div className="navbtns md:w-2/3  sm:hidden sm:items-center justify-center text-center md:justify-normal">
-                            <div className="navbtn1 px-5 py-3 text-[#68696F] text-base font-medium" onClick={() => setShowNav(true)}><span className="p-2 hover:bg-[#F93800]/20 rounded-md">Tap to see more pages</span></div>
+                            <div className="navbtn1 px-5 py-3 text-[#68696F] text-base font-medium" onClick={() => setShowNav(true)}>
+                                <span className=" flex justify-center text-center p-2 text-2xl hover:bg-[#F93800]/20 rounded-md">
+                                Tap to see more pages 
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+                                </span>
+                                </div>
                         </div>
                     </>
                     : <>
