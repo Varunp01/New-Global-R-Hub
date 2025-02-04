d6f99344fd53839a1552b611eb5cf4843b353eac
Varun Pandey <varunp5324@gmail.com>
2025-02-04 15:16:22 +0530

    first commit

--- src/pages/Home/Section8.jsx
+++ src/pages/Home/Section8.jsx
@@ -1,3 +1,6 @@
+
+
+
 const HSec8 = () => {
     return (
         <>
@@ -7,7 +10,7 @@ const HSec8 = () => {
                     <div className="fontInter font-bold text-black text-4xl pt-4">Hear From Our Students</div>
                     <div className="customScroll flex overflow-x-scroll">
 
-                        <div className=" md:w-[35%] w-[80%] my-5 mx-2 bg-[#0F172A] rounded-3xl">
+                        <div className="  w-[500px] min-w-[300px]   my-5 mx-2 bg-[#0F172A] rounded-3xl">
                             <div className="md:flex p-5 w-100% items-center justify-center">
                                 <div className=" text-center mx-auto rounded-[100%] md:w-[20%] w-[30%] mb-2 border-2 border-white aspect-square bg-red-300 bg-cover bg-center bg-[url('https://i.ibb.co/syyYQn4/Dr-Deepti-Singh.png')]"></div>
                                 <div className="pl-6 w-[80%] items-center">
@@ -35,17 +38,17 @@ const HSec8 = () => {
                             <div className="px-7">
                                 <hr className="text-black bg-black border-1 border-gray-600" color="black" />
                             </div>
-                            <div className="fontInter md:px-10 px-2 py-5 md:text-xl text-base font-bold text-white">
+                            <div className="fontInter md:px-10 px-2 py-5 md:text-base text-base font-bold text-white">
                                 During my MS program, I had the privilege of working as a Research Assistant in Cyber Security under Prof. Shivendu’s exceptional mentorship. <br /><br /> His deep knowledge and dedication to student success not only enhanced my empirical and analytical skills but also instilled confidence in my ability to tackle complex research problems.
                             </div>
                         </div>
 
-                        <div className=" md:w-[35%] w-[80%] my-5 mx-2 bg-[#0F172A] rounded-3xl">
+                        <div className=" w-[500px] min-w-[300px]   my-5 mx-2 bg-[#0F172A] rounded-3xl">
                             <div className="md:flex p-5 w-100% items-center justify-center">
-                                <div className=" text-center mx-auto rounded-[100%] md:w-[20%] w-[30%] mb-2 border-2 border-white aspect-square bg-red-300 bg-cover bg-center bg-[url('https://i.ibb.co/syyYQn4/Dr-Deepti-Singh.png')]"></div>
+                                <div className=" text-center mx-auto rounded-[100%] md:w-[20%] w-[30%] mb-2 border-2 border-white aspect-square bg-red-300 bg-cover bg-center bg-[url('https://i.ibb.co/99tKwpK/G-Dia-Reddy.png')]"></div>
                                 <div className="pl-6 w-[80%] items-center">
-                                    <div className="fontInter font-bold text-2xl text-white">Anjana Ruttala</div>
-                                    <div className="fontInter font-semibold text-sm text-[#818181]">Senior Data Engineer, Modmed</div>
+                                    <div className="fontInter font-bold text-2xl text-white">G. Dia Reddy</div>
+                                    <div className="fontInter font-semibold text-sm text-[#818181]">B.A in Economics</div>
                                     <div className=" flex items-center">
                                         <svg class="w-6 h-6 ms-1 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                                             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -68,15 +71,50 @@ const HSec8 = () => {
                             <div className="px-7">
                                 <hr className="text-black bg-black border-1 border-gray-600" color="black" />
                             </div>
-                            <div className="fontInter md:px-10 px-2 py-5 md:text-xl text-base font-bold text-white">
-                                During my MS program, I had the privilege of working as a Research Assistant in Cyber Security under Prof. Shivendu’s exceptional mentorship. <br /><br /> His deep knowledge and dedication to student success not only enhanced my empirical and analytical skills but also instilled confidence in my ability to tackle complex research problems.
+                            <div className="fontInter md:px-10 px-2 py-5 md:text-base text-base font-bold text-white">
+                                Working on a research project with Dr. Shrivastava was an incredible experience. From the very beginning, she made complex topics like Non-Performing Assets (NPAs) completely accessible walking me through the process of extracting and analyzing data from bank reports, organizing it into a time series format that was both logical and manageable.
                             </div>
                         </div>
-                        
-                        <img src="https://i.ibb.co/Fs1tQBZ/Group-83.png" alt="Card1" className=" md:w-[40%] w-[80%] py-5 px-5" />
+
+                        <div className=" w-[500px] min-w-[300px]   my-5 mx-2 bg-[#0F172A] rounded-3xl">
+                            <div className="md:flex p-5 w-100% items-center justify-center">
+                                <div className=" text-center mx-auto rounded-[100%] md:w-[20%] w-[30%] mb-2 border-2 border-white aspect-square bg-red-300 bg-cover bg-center bg-[url('https://i.ibb.co/BqNvrTy/Anjana-Ruttala.png')]"></div>
+                                <div className="pl-6 w-[80%] items-center">
+                                    <div className="fontInter font-bold text-2xl text-white">Dr. Deepti Singh</div>
+                                    <div className="fontInter font-semibold text-sm text-[#818181]">Ph. D. in Business Administration</div>
+                                    <div className=" flex items-center">
+                                        <svg class="w-6 h-6 ms-1 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                        </svg>
+                                        <svg class="w-6 h-6 ms-1 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                        </svg>
+                                        <svg class="w-6 h-6 ms-1 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                        </svg>
+                                        <svg class="w-6 h-6 ms-1 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                        </svg>
+                                        <svg class="w-6 h-6 ms-1 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="px-7">
+                                <hr className="text-black bg-black border-1 border-gray-600" color="black" />
+                            </div>
+                            <div className="fontInter md:px-10 px-2 py-5 md:text-base text-base font-bold text-white">
+                                Dr. Shivendu, was the chair of my dissertation committee during my doctoral program in information systems at USF. His research mentorship, constant support and deep knowledge of research methods helped me in growing into an academic. Dr. Shivendu has deep commitment towards student success and his professional as well as academic journey is inspiring.
+                            </div>
+                        </div>
+
+                        {/* <img src="https://i.ibb.co/Fs1tQBZ/Group-83.png" alt="Card1" className=" md:w-[40%] w-[80%] py-5 px-5" /> */}
                         {/* <img src="https://i.ibb.co/Fs1tQBZ/Group-83.png" alt="Card1" className=" md:w-[40%] w-[80%] py-5 px-5"/>
                 <img src="https://i.ibb.co/Fs1tQBZ/Group-83.png" alt="Card1" className=" md:w-[40%] w-[80%] py-5 px-5"/> */}
                     </div>
+                    <hr />
+                    
                 </div>
             </div>
         </>
