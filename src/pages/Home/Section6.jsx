994fa031a040f9ea3f3b56ce5b2daf9e81d52d38
Varun Pandey <varunp5324@gmail.com>
2025-02-04 12:31:15 +0530

    first commit

--- src/pages/Home/Section6.jsx
+++ src/pages/Home/Section6.jsx
@@ -7,7 +7,7 @@ const HSec6 = () => {
             <div className="flex justify-center fontInter font-bold text-black text-5xl py-2 px-6">Do Your Know When to Start?</div>
             <div className="flex justify-around flex-wrap py-5 px-6 my-5">
 
-                <div class="overflow-hidden hover:shadow-2xl hover:shadow-[#0066ff75] w-[300px] h-[450px] mt-6 border-2 rounded-3xl max-w-sm shadow-xl text-center  bg-cover bg-center bg-[url('https://i.ibb.co/QXqSd37/6349af887d89270fdd60042011f3646f.jpg')]">
+                <div class="overflow-hidden hover:shadow-2xl hover:shadow-[#0066ff75] w-[300px] h-[450px] mt-6 border-2 rounded-3xl max-w-sm shadow-xl text-center  bg-cover bg-center bg-[url('https://i.ibb.co/7JSGv0q0/smiling-young-school-boy-wearing-backpack-with-cap-holding-books-crossing-fingers-isolated-white-wal.jpg')]">
                     <div className="rounded-3xl mt-[100px] h-[100%] pt-20 px-2 pb-5 bg-gradient-to-t from-white via-white to-transparent">
                         <div class="mt-[55px] block text-left  text-wrap w-[100%] mb-5">
                             <div className="fontInter font-bold text-[#000000] text-2xl">Young Scholar <br /> (Grades 6-8)</div>
@@ -16,7 +16,7 @@ const HSec6 = () => {
                     </div>
                 </div>
                 
-                <div class="overflow-hidden hover:shadow-2xl hover:shadow-[#0066ff75] w-[300px] h-[450px] mt-6 border-2 rounded-3xl max-w-sm shadow-xl text-center  bg-cover bg-center bg-[url('Global\public\school2.jpg')]">
+                <div class="overflow-hidden hover:shadow-2xl hover:shadow-[#0066ff75] w-[300px] h-[450px] mt-6 border-2 rounded-3xl max-w-sm shadow-xl text-center  bg-cover bg-center bg-[url('https://i.ibb.co/wr4fpp0j/handsome-indian-young-boy-wearing-white-shirt-red-tie.jpg')]">
                     <div className="rounded-3xl mt-[100px] h-[100%] pt-20 px-2 pb-5 bg-gradient-to-t from-white via-white to-transparent">
                         <div class="mt-[55px] block text-left  text-wrap w-[100%] mb-5">
                             <div className="fontInter font-bold text-[#000000] text-2xl">High School Scholar <br /> (Grades 9-12)</div>
